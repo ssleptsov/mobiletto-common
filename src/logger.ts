3429a6fd178c4b4cdd0943b6b8f526cfe2e10d8b
Jonathan Cobb <jonathan@kyuss.org>
2023-07-05 23:07:18 -0400

    adjust indent

--- src/logger.ts
+++ src/logger.ts
@@ -1,32 +1,32 @@
 import * as winston from "winston";
 
 export const logger = winston.createLogger({
-  levels: winston.config.npm.levels,
-  level: process.env.MOBILETTO_LOG_LEVEL || "error",
-  format: winston.format.combine(
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
-    winston.format.printf((info: any) => {
-      return `${info.timestamp} [${info.level.toUpperCase()}] ${info.message}`;
-    })
-  ),
-  transports: process.env.MOBILETTO_LOG_FILE
-    ? [
-        new winston.transports.File({
-          filename: process.env.MOBILETTO_LOG_FILE,
-        }),
-      ]
-    : [
-        new winston.transports.Console({
-          stderrLevels: Object.keys(winston.config.npm.levels),
-        }),
-      ],
+    levels: winston.config.npm.levels,
+    level: process.env.MOBILETTO_LOG_LEVEL || "error",
+    format: winston.format.combine(
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
+        winston.format.printf((info: any) => {
+            return `${info.timestamp} [${info.level.toUpperCase()}] ${info.message}`;
+        })
+    ),
+    transports: process.env.MOBILETTO_LOG_FILE
+        ? [
+              new winston.transports.File({
+                  filename: process.env.MOBILETTO_LOG_FILE,
+              }),
+          ]
+        : [
+              new winston.transports.Console({
+                  stderrLevels: Object.keys(winston.config.npm.levels),
+              }),
+          ],
 });
 
 export const setLogLevel = (level: string) => {
-  logger.level = level;
+    logger.level = level;
 };
 
 export const setLogTransports = (transports: any[]) => {
-  logger.transports.splice(0, logger.transports.length);
-  logger.transports.push(...transports);
+    logger.transports.splice(0, logger.transports.length);
+    logger.transports.push(...transports);
 };
